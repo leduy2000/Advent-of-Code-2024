c902dd5e6fe6931d0f54cd4f15c179c06808f0c7
Lê Duy <leduy141000@gmail.com>
2024-12-01 13:13:48 +0700

    day 1

---
+++ day1.1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+#define int long long
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// order_of_key (k) : Number of items strictly smaller than k
+// find_by_order(k) : K-th element in a set (counting from zero)
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+	vector<int> a, b;
+	for (int i = 0; i < 2000; i++) {
+		int x;
+		cin >> x;
+		if (i % 2 == 0) {
+			b.push_back(x);
+		} else {
+			a.push_back(x);
+		}
+	}
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+
+	int ans = 0;
+	for (int i = 0; i < 1000; i++) {
+		ans += abs(a[i] - b[i]);
+	}
+	cout << ans << '\n';
+}
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	solve();
+	return 0;
+}
