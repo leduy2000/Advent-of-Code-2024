f2b7ee6e395c9226396cbb94a168ecedcb1ac9f6
Duy <duy.le@platform.inc>
2024-12-03 10:50:00 +0700

    sf

--- day2.2.cpp
+++ day2.2.cpp
@@ -61,11 +61,6 @@ void solve() {
 signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    // int t;
-    // cin >> t;
-    // while (t--) {
-    //     solve();
-    // }
     solve();
     return 0;
 }
